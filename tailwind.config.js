b83ccf1c1358d9a9ff8d230d3a309a3a303e0474
Elie <eliebm17@live.com>
2024-04-27 11:44:11 +0300

    create project with tailwind daisy UI and font awesome

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,ts}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: ["light", "dark"],
+  },
+  plugins: [require("daisyui")],
+};
